0cc283fd94bb6e2dd6a7e4b9de46cbd29f3b65a7
tomoki <tomoki171923@gmail.com>
2021-08-10 04:34:58 +0900

    create cloudwatch event module

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "aws_cloudwatch_event_rule" "this" {
+  name                = var.name
+  description         = var.description
+  schedule_expression = var.schedule_expression
+  is_enabled          = true
+  tags                = var.tags
+}
+resource "aws_cloudwatch_event_target" "this" {
+  rule      = aws_cloudwatch_event_rule.this.name
+  target_id = "lambda"
+  arn       = var.lambda_alias_arn
+  input     = var.input
+}
+resource "aws_lambda_permission" "this" {
+  statement_id  = "allow_cloudwatch_${aws_cloudwatch_event_rule.this.name}_to_invoke_${var.lambda_function_name}_${var.lambda_alias_name}"
+  action        = "lambda:InvokeFunction"
+  function_name = "${var.lambda_function_name}:${var.lambda_alias_name}"
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.this.arn
+}
